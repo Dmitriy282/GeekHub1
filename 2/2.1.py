7477c67f4f18c4efdda0cf0aa8260cfa794097c8
Dmitriy282 <74493194+Dmitriy282@users.noreply.github.com>
2021-11-19 15:27:55 +0200

    Update 2.1.py

--- 2/2.1.py
+++ 2/2.1.py
@@ -1,5 +1,6 @@
 #Елементами списку повинні бути як рядки, так і числа.
- 
-elements = ['1', '2', 'test', '10', 'foo']
-str1 ="'"+"".join(elements)+"'"
-print(str1)
+values = [1,2,'test',10,'foo']
+x = ""
+for i in values:
+  x += str(i)
+print(x)
