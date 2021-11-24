b34ab6f37a9cfed47e35a6f18cfad4f1f9ced94f
Dmitriy282 <74493194+Dmitriy282@users.noreply.github.com>
2021-11-24 13:52:10 +0200

    Update 5.py

--- 3/5.py
+++ 3/5.py
@@ -5,14 +5,21 @@
 #-  x < y;       вiдповiдь - у бiльше нiж х на z
 #-  x == y.      вiдповiдь - х дорiвнює z
 def construct():
-    x = int(input("Введіть число: "))
-    y = int(input("Введіть число: "))
+    x = int(input("Введіть число X: "))
+    y = int(input("Введіть число Y: "))
+    print('X=' + str(x))
+    print('Y=' + str(y))
     if x < y:
+        z = x - y
         print("у бiльше нiж х на z")
+        print('Z=' + str(z))
     elif x > y:
+        z = x - y
         print("х бiльше нiж у на z")
+        print('Z=' + str(z))
     else:
         x = y
         print("х дорiвнює z")
+
 
 construct()
