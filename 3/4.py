b410d44cded28154cf769520df49b586db769c67
Dmitriy282 <74493194+Dmitriy282@users.noreply.github.com>
2021-11-24 13:45:43 +0200

    Update 4.py

--- 3/4.py
+++ 3/4.py
@@ -1,10 +1,20 @@
 #Створiть 3 рiзних функцiї (на ваш вибiр). Кожна з цих функцiй повинна повертати якийсь результат. Також створiть четверу ф-цiю, яка в тiлi викликає 3 попереднi, обробляє повернутий ними результат та також повертає результат. Таким чином ми будемо викликати 1 функцiю, а вона в своєму тiлi ще 3
+import math
 def func():
-    def func1():
-        def func2():
-            def func3():
-                print("Hello World")
-            func3()
-        func2()
-    func1()
+    def theorem_Pythagorean():
+        def square():
+            def students():
+                print("Hello students!")
+            students()
+            a = float(input("Введіть довжину сторони квадрата: "))
+            s = a ** 2
+            S = "Площа квадрата: " + str(s)
+            print(S)
+        square()
+        a = float(input("Введіть довжину першого катета прямокутного трикутника: "))
+        b = float(input("Введіть довжину другого катета прямокутного трикутника: "))
+        c = (a**2) + (b**2)
+        d = math.sqrt(c)
+        print("Гіпотенуза дорівнює: "+ str(d))
+    theorem_Pythagorean()
 func()
