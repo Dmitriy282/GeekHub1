4bd3e9f9cc874d4d80d3774aa3a6e21d490538f1
Dmitriy282 <74493194+Dmitriy282@users.noreply.github.com>
2021-11-27 17:08:00 +0200

    Update 5.py

--- 4/5.py
+++ 4/5.py
@@ -1,8 +1,9 @@
 #Написати функцію < fibonacci >, яка приймає один аргумент і виводить всі числа Фібоначчі, що не перевищують його.
-n = int(input("Введіть число:"))
 def fib(n):
     if (n==1 or n==2):
         return 1
+    elif n==0:
+        return 0
     return fib(n-2)+fib(n-1)
 
-print(fib(n))
+[print(fib(n)) for n in range (int(input("Введіть число: ")))]
