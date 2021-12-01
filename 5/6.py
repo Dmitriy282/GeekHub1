878afa6ff1001e0fc22652ba57648383011a338f
Dmitriy282 <74493194+Dmitriy282@users.noreply.github.com>
2021-12-01 20:54:53 +0200

    Update 6.py

--- 5/6.py
+++ 5/6.py
@@ -2,10 +2,15 @@
 #P.S. Повинен вертатись генератор.
 #P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
 def my_range(start,stop=0,step=1):
-    while start<stop:
-        yield start
-        start += step
-    if stop<start:
-        stop += 1
-for i in my_range(1,20,8):
+    if start < stop:
+        while start<stop:
+            yield start
+            start += step
+    else:
+        while stop < start:
+            yield stop
+            stop+=1
+for i in my_range(1,10,3):
     print(i)
+
+
