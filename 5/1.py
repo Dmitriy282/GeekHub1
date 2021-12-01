a976bd57b0fc6d75ee9015b8532dfe23668630d2
Dmitriy282 <74493194+Dmitriy282@users.noreply.github.com>
2021-12-01 22:39:21 +0200

    Update 1.py

--- 5/1.py
+++ 5/1.py
@@ -11,21 +11,16 @@ def f(username, password, silent=False):
                  ['dggsge', 'dfsef'],
                  ['admin', 'admin'],
                  ['Geekhub', 'forever']]
-    list_user1 = [i[0] for i in data]
-    passwords1 = [i[1] for i in data]
-    if username in list_user1 and password in passwords1 and silent == False:
+    if [username, password] in data:
         return True
-    elif username not in list_user1 and password not in passwords1 and silent == True:
+    elif [username, password] not in data and silent == True:
         return False
-    elif username not in list_user1 or password not in passwords1 and silent == True:
-        return False
-    elif username in list_user1 and password in passwords1 and silent == True:
-        return True
     else:
-        raise LoginException('Невірно логін або пароль')
+        raise LoginException('Неправильний логін або пароль')
 
-print(f('Geekhub','forever'))
+
+print(f('admin','admin',False))
 print(f('ап', 'sf',True))
-print(f('admin','admin',True))
 print(f('dsfa','fsasfa',True))
-print(f('Geekhub','sdsdadfsaf'))
+print(f('Geekhub','admin'))
+
